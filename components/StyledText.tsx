8f21c271d1021e9152ce3abfd49f03e7760eeea8
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-29 18:54:50 +0300

    remove unused

--- components/StyledText.tsx
+++
@@ -1,7 +0,0 @@
-import { Text } from 'react-native';
-
-export function StyledText(props: any) {
-  return (
-    <Text {...props} style={[props.style, { fontFamily: 'nunito-regular' }]} />
-  );
-}
