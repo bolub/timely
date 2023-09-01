7d20a85e5001d165d1d38a2c46c4ace49e72dbad
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-09-01 17:57:36 +0300

    Add GluestackProvider

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,12 @@
 import { HomePage } from '../containers/home/HomePage';
+import { GluestackUIProvider, config } from '@gluestack-ui/themed';
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <>
+      <GluestackUIProvider config={config.theme}>
+        <HomePage />
+      </GluestackUIProvider>
+    </>
+  );
 }
