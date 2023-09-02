6529708460fd4442bb3f76029a4601a9ee73bc99
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-09-02 17:02:40 +0300

    change url scheme to https

--- api/timezone.ts
+++ api/timezone.ts
@@ -1,13 +1,22 @@
 import axios from 'axios';
+import { storeErrors } from './errors';
 
 export const getTimeData = async (timezone: string) => {
-  const response = await axios.get(
-    `http://worldtimeapi.org/api/timezone/${timezone}`
-  );
+  try {
+    const response = await axios.get(
+      `https://worldtimeapi.org/api/timezone/${timezone}`
+    );
 
-  return response.data as {
-    timezone: string;
-    datetime: Date;
-    utc_datetime: string;
-  };
+    return response.data as {
+      timezone: string;
+      datetime: Date;
+      utc_datetime: string;
+    };
+  } catch (error) {
+    await storeErrors({
+      timezone,
+      error,
+    });
+    throw error;
+  }
 };
